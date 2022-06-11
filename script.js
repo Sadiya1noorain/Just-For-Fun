9b6cbe04921ce33937093f51ac172254dff62aa5
Sadiya Noorain <53908125+Sadiya1noorain@users.noreply.github.com>
2022-06-11 21:12:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -42,8 +42,8 @@ move.addEventListener('mouseover', function () {
 });
 
 move.addEventListener('touchstart', function () {
-  modal.style.top = Math.random() * (screen.innerHeight / 4) + "px";
-  modal.style.left = Math.random() * (screen.innerWidth / 4) + "px";
+  modal.style.top = Math.random() * (screen.innerHeight) + "px";
+  modal.style.left = Math.random() * (screen.innerWidth) + "px";
 });
 
 yes.addEventListener('click', function () {
